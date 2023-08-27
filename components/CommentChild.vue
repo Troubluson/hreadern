80c50ebe055f63a53780ea7f27820fe02fbe7ca3
Henrik Niskanen <h.niskanen99@gmail.com>
2023-08-27 16:11:34 +0300

    feat: add component for comments

---
+++ components/CommentChild.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Comment } from "types";
+
+const props = defineProps({
+  comment: Object,
+  replies: Array,
+});
+const comment = props.comment as Comment;
+const replies = props.replies as Comment[];
+const [newFirst, ...newReplies] = replies;
+</script>
+
+<template>
+  <div v-if="comment" class="ml-6 pb-0">
+    <p class="border p-2" v-html="comment.text"></p>
+    <CommentChild :replies="newReplies" :comment="newFirst" />
+  </div>
+</template>
