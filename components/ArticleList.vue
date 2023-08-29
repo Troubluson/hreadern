76dde7bb4d63f69939730e12289e3c01cbc6fc5b
Henrik Niskanen <h.niskanen99@gmail.com>
2023-08-29 19:14:24 +0300

    style: sort button styling

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -16,23 +16,20 @@ const { data: storyIds, pending } = useFetch<number[]>(fetchUrl, {
 <template>
   <div>
     <div class="flex">
-      <div
-        class="inline-flex rounded-md shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-        role="group"
-      >
+      <div class="inline-flex" role="group">
         <button
           @click="setSortBy('top')"
           type="button"
-          class="inline-block rounded-l bg-primary-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 focus:outline-none focus:ring-0 active:bg-primary-700"
-          data-te-ripple-init
-          data-te-ripple-color="light"
+          :class="{ 'border rounded border-emerald-700': sortBy === 'top' }"
+          class="inline-block px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 focus:outline-none focus:ring-0 hover:bg-zinc-950 active:bg-zinc-900"
         >
           Top
         </button>
         <button
           @click="setSortBy('new')"
           type="button"
-          class="inline-block bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 focus:outline-none focus:ring-0 active:bg-primary-700"
+          :class="{ 'border rounded border-emerald-700': sortBy === 'new' }"
+          class="inline-block px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 focus:outline-none focus:ring-0 hover:bg-zinc-950 active:bg-zinc-900"
         >
           New
         </button>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container mx-auto px-4">
-    <h1 class="text-4xl text-slate-300">Articles</h1>
     <ArticleList />
   </div>
 </template>
